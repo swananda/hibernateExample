ada04c777d84ba6200675fcba25de4e13e121237
user <user@user-PC>
2017-06-12 15:21:35 +0530

    employe.java committing

---
+++ 12/hibernateexample/src/main/java/com/hibjava/Employee.java
@@ -0,0 +1,71 @@
+package com.hibjava;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="emptable")
+public class Employee{
+	
+	@Id
+	private int id;
+	@Column
+	private String firstName;
+	@Column
+	private String lastName;
+	@Column
+	private int salary;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public Employee(int id, String firstName, String lastName, int salary) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.salary = salary;
+	}
+
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", salary=" + salary
+				+ "]";
+	}
+
+}
